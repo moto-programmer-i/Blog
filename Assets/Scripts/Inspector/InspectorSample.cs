67a86406c92e109f128dc9c712409ccc43fbfc44
元プログラマi <99943229+moto-programmer-i@users.noreply.github.com>
2024-07-26 16:26:01 +0900

    変数の順番変更

--- Assets/Scripts/Inspector/InspectorSample.cs
+++ Assets/Scripts/Inspector/InspectorSample.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class InspectorSample : MonoBehaviour
 {
-    // エラー
-    [SerializeField]
-    private List<Plane> planes = new();
-
-
     // 内部の変数も表示される
     [SerializeField]
     private List<Serializable> serializables = new();
@@ -17,6 +12,13 @@ public class InspectorSample : MonoBehaviour
     // 他への参照となり、内部の変数は表示されない
     [SerializeField]
     private List<MonoBehaivorEx> monoBehaivorEx = new();
+
+
+    // エラー
+    [SerializeField]
+    private List<Plane> planes = new();
+
+
 
     // Start is called before the first frame update
     void Start()
